aba402f07d6296d79e703796bcd47345b78d6a08
LuisFcamo <luis1170da@gmail.com>
2024-09-03 10:42:32 -0300

    leetcode problems 2710

--- LEETCODE/2185.py
+++ LEETCODE/2185.py
@@ -3,7 +3,7 @@ class Solution:
     def prefixCount(self, words: List[str], pref: str) -> int:
         qtd = 0
         for w in words:
-            if pref in w:
+            if w.startswith(pref):
                 qtd+=1
         return qtd
 print(Solution().prefixCount(["leetcode","win","loops","success"], "code"))
